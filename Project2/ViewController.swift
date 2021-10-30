20cc261fc76f5a036ffe61c12449995443433e31
RazanAlthkafi <Razan.Althkafi.med.s@tuwaiq.edu.sa>
2021-10-30 15:38:32 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Roaa aljohani on 24/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
